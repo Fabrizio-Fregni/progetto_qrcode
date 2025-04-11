47497b1d583ecc454768777dfa62a51a71673149
Fabrizio-Fregni <fregni.fabrizio@fitstic-edu.com>
2025-04-11 15:02:38 +0000

    Aggiornato README.md e aggiunto init.py

--- azienda/urls.py
+++ azienda/urls.py
@@ -18,7 +18,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
-from .gestione import views
 
 
 urlpatterns = [
