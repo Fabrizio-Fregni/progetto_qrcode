733d097041994601202fd0ca1f71d4f50a380eae
Fabrizio-Fregni <fregni.fabrizio@fitstic-edu.com>
2025-04-11 09:42:55 +0000

    Aggiornato README.md e aggiunto init.py

--- azienda/gestione/admin.py
+++ azienda/gestione/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Cliente, Azienda, Sede, Prodotto
 
-# Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Azienda)
+admin.site.register(Sede)
+admin.site.register(Prodotto)
